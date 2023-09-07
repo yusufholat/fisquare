e34542571d0d8e6a53203579cbd08490b030e43f
Ziroo <yusufholat@gmail.com>
2023-09-08 02:40:18 +0300

    optimize collision detection

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -7,17 +7,22 @@ using UnityEngine;
 public class ArrowController : MonoBehaviour
 {
     [SerializeField] private float arrowSpeed;
+  
+    [SerializeField] private LayerMask hitLayers;
+    [SerializeField] private Transform frontPoint;
+    [SerializeField] private float arrowRadius;
+
+    private Collider2D hitColl;
 
     void FixedUpdate(){
         transform.Translate(transform.right * arrowSpeed * Time.deltaTime, Space.World);
+        hitColl = Physics2D.OverlapCircle(frontPoint.position, arrowRadius, hitLayers);
+        if(hitColl != null) Destroy(gameObject);
     }
     
-    
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnDrawGizmos()
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            Destroy(gameObject);
-        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(frontPoint.position, arrowRadius);
     }
 }
