976d34a72ac41018c6187fdd7e9381d69c4c9a71
Ziroo <yusufholat@gmail.com>
2023-09-07 21:09:05 +0300

    arrowcontrol

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -1,18 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
     [SerializeField] private float arrowSpeed;
 
-    void Start(){
-
-    }
-
     void FixedUpdate(){
         transform.Translate(transform.right * arrowSpeed * Time.deltaTime, Space.World);
     }
-
+    
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
